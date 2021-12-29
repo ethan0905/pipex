4a731e215b70e01e2bcefc72620d0a4a32b97c53
esafar ~ c2h6 <77629339+ethan0905@users.noreply.github.com>
2021-12-29 19:33:19 +0100

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -23,10 +23,26 @@ int	open_file(char *file_name, int mode)
 			ft_putstr_fd(": No such file or directory\n", STDERR);
 			return (STDIN);
 		}
-		return (open(file_name, O_RDONLY));
+		else if (access(file_name, R_OK))
+		{
+			ft_putstr_fd("pipex: ", STDERR);
+			write(STDERR, file_name, str_search(file_name, '\0'));
+			ft_putstr_fd(": Wrong rights on file\n", STDERR);
+			return (STDIN);
+		}
+		return (open(file_name, O_RDONLY | O_TRUNC, 0644));
 	}
 	else if (mode == OUTFILE)
+	{
+		// if (access(file_name, W_OK))
+		// {
+		// 	ft_putstr_fd("pipex: ", STDERR);
+		// 	write(STDERR, file_name, str_search(file_name, '\0'));
+		// 	ft_putstr_fd(": Wrong rights on file\n", STDERR);
+		// 	exit(0);
+		// }
 		return (open(file_name, O_CREAT | O_WRONLY | O_TRUNC, 0644));
+	}
 	return (-1);
 }
 
@@ -47,25 +63,25 @@ void	exec_cmd(char *cmd, char **env)
 	exit(127);
 }
 
-void	proceed_processes(char *cmd, char **env, int fdin)
+void	proceed_processes(char *cmd, char **env, int fdin, t_data *data)
 {
 	pid_t	pid;
-	int		pipefd[2];
 
-	pipe(pipefd);
+	pipe(data->pipefd);
 	pid = fork();
+	printf("pid = %d", pid);
 	if (pid)
 	{
-		close(pipefd[1]);
-		dup2(pipefd[0], STDIN);
-		close(pipefd[0]);
+		close(data->pipefd[1]);
+		dup2(data->pipefd[0], STDIN);
+		close(data->pipefd[0]);
 		waitpid(pid, NULL, 0);
 	}
 	else
 	{
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT);
-		close(pipefd[1]);
+		close(data->pipefd[0]);
+		dup2(data->pipefd[1], STDOUT);
+		close(data->pipefd[1]);
 		if (fdin == STDIN)
 			exit(1);
 		else
@@ -73,29 +89,53 @@ void	proceed_processes(char *cmd, char **env, int fdin)
 	}
 }
 
+void	check_error(char *arg, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i])
+		i++;
+	if (i == 0)
+	{
+		ft_putstr_fd("pipex: Command not found\n", 2);
+		close(data->fdin);
+		close(data->fdout);
+		close(STDIN);
+		close(STDOUT);
+		close(STDERR);
+		exit(0);
+	}
+	return ;
+}
+
 int	main(int ac, char **av, char **env)
 {
 	int	i;
-	int	fdin;
-	int	fdout;
+	t_data data;
 
 	i = 2;
 	if (ac == 5)
 	{
-		fdin = open_file(av[1], INFILE);
-		fdout = open_file(av[ac - 1], OUTFILE);
-		dup2(fdin, STDIN);
-		dup2(fdout, STDOUT);
+		data.fdin = open_file(av[1], INFILE);
+		data.fdout = open_file(av[ac - 1], OUTFILE);
+		dup2(data.fdin, STDIN);
+		dup2(data.fdout, STDOUT);
 		while (i < ac - 2)
 		{
+			check_error(av[i], &data);
 			if (i == 2)
-				proceed_processes(av[i++], env, fdin);
+				proceed_processes(av[i++], env, data.fdin, &data);
 			else
-				proceed_processes(av[i++], env, STDOUT);
+				proceed_processes(av[i++], env, STDOUT, &data);
 		}
 		exec_cmd(av[i], env);
 	}
 	else
 		ft_putstr_fd("Wrong number of arguments.\n", STDERR);
+	close(data.pipefd[0]);
+	close(data.pipefd[1]);
+	close(data.fdin);
+	close(data.fdout);
 	return (0);
 }
